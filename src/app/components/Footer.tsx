0c4e909e8cc775acccaa90ee75e2eba4c3aef7cd
Jonas090405 <149697759+Jonas090405@users.noreply.github.com>
2026-05-12 16:16:37 +0200

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -84,7 +84,7 @@ export function Footer() {
               {EMAIL_ADDRESS}
             </a>
             <a
-              href="https://www.linkedin.com/in/jonas-gissler-37b1482b0/"
+              href="https://www.linkedin.com/company/gissler-webdesign/"
               target="_blank"
               rel="noreferrer"
               aria-label="LinkedIn Profil von Jonas Gissler"
